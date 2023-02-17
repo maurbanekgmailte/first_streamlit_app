2263eb579bfe022038c1fb73c5518acfd6e5701c
maurbanekgmailte <124870124+maurbanekgmailte@users.noreply.github.com>
2023-02-17 13:58:21 +0100

    Update streamlit_app.py
    format in fruityvice to dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,3 +22,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# normalize fruityvice response
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# show in table
+streamlit.dataframe(fruityvice_normalized)
